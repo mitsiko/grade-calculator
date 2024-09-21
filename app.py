81fc109f54195948609a0ddab306e2655c91e709
Lee Michiko Garcia <michikogarciasp@gmail.com>
2024-09-22 02:53:33 +0800

    Removed templates folder

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-from flask import Flask, render_template, request
 
 # Initialize the Flask application
 app = Flask(__name__, static_url_path='/static', static_folder='docs/static', template_folder='docs')
